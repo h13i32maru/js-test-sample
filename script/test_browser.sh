520fc7c6d8ac532aa38a25c27f11804fa20b6951
h13i32maru <h13i32maru@gmail.com>
2014-12-29 15:08:23 +0900

    fix format.

--- script/test_browser.sh
+++ script/test_browser.sh
@@ -3,4 +3,4 @@
 ./node_modules/.bin/traceur --out build/test/all_test.js $(find test/ -name '*_test.js')
 ./node_modules/.bin/espower build/test/all_test.js > build/test/espowered_all_test.js
 
-open test/index.html
+open -a 'Google Chrome' test/index.html
